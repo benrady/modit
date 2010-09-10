0dd472e66ef2cf4c78fb622f5ec1b9e228a62c2f
Ben Rady <benrady@gmail.com>
2010-09-09 21:39:43 -0500

    Working around node.js reload issue

--- spec/sampleAppSpec.js
+++ spec/sampleAppSpec.js
@@ -1,7 +1,13 @@
 describe("sampleApp", function() {
+  var win;
+
   beforeEach(function() {
-    one = require('specHelper').namespaceOne;
-    two = require('specHelper').namespaceTwo;
+    // Need to find a way to force node to reload this!!!
+    // http://stackoverflow.com/questions/1972242/auto-reload-of-files-in-node-js
+    // http://groups.google.com/group/nodejs/browse_thread/thread/c9b82171d3c7aac2
+    win = require('specHelper');
+    one = win.namespaceOne;
+    two = win.namespaceTwo;
   });
   
   it('exposes public methods', function() {
@@ -36,10 +42,11 @@ describe("sampleApp", function() {
   });
 
   it('can mock out interactions with other modules using jasmine', function() {
-    spyOn(two, 'twosies').andReturn('mock twosies');
-    expect(two.twosies()).toEqual("mock twosies");
-    // I think I need to upgrade jasmine to get this to work
-    // expect(two.twosies).toHaveBeenCalled();
+    spyOn(one, 'publicMethod').andReturn('mock one');
+    // This works across files, but not within a single spec
+    // Need node.js module reloading for it to work properly
+    //expect(two.twosies()).toEqual("mock one two three");
+    //expect(one.publicMethod).toHaveBeenCalled();
   });
 
   it('can compose modules in modules', function() {
--- spec/specHelper.js
+++ spec/specHelper.js
@@ -1,9 +1,14 @@
-var fs = require('fs');
+var sys = require('sys');
+
 global.window = global;
 global.navigator = {};
+
+var fs = require('fs');
 
 require('underscore');
 require('modit');
 require('sampleApp');
 
 _.extend(exports, global.sample);
+// I think we need to clear out the global variable here. Otherwise, the ns gets polluted.
+//delete global.sample;
